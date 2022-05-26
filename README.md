5c2187dd05c081d40c399a5708281d705de4825a
motojouya <motojouya@gmail.com>
2022-05-27 00:20:33 +0900

    コマンドの利用イメージ作成

--- README.md
+++ README.md
@@ -2,3 +2,45 @@
 
 A Board Game like "Tactics Ogre: Let Us Cling Together", computer SRPG game.
 
+## design
+
+### command
+- check
+  初期入力データのチェック
+- start
+  ゲームの開始
+
+#### command action
+- action
+  技選択 -> ターゲット選択 -> 実行 -> ターンエンド
+  勝敗が決まれば結果
+  決まってなければ次のユニットを表示
+- next
+  actionを行わず次のユニットへ
+- status
+  - hp
+  - mp
+  - 順番
+- parson
+  人を選択
+- quit
+  ゲーム終了
+
+### data
+初期入力データ
+こんな感じを想定
+TypeScriptで型を定義したい
+
+```
+{
+  "name": {
+    "blessing": "",
+    "race": "",
+    "main_weapon": "",
+    "sub_weapon": "",
+    "armor": ""
+  },
+  ...
+}
+```
+
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,54 @@
-#!/usr/bin/env node
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+import inquirer from 'inquirer';
+import figlet from 'figlet';
+// #!/usr/bin/env node
 
-yargs(hideBin(process.argv))
-  .command('serve [port]', 'start the server', (yargs) => {
-    return yargs
-      .positional('port', {
-        describe: 'port to bind on',
-        default: 5000
-      })
-  }, (argv) => {
-    if (argv.verbose) console.info(`start server on :${argv.port}`)
-    serve(argv.port)
-  })
-  .option('verbose', {
-    alias: 'v',
-    type: 'boolean',
-    description: 'Run with verbose logging'
-  })
-  .parse()
+const test = () => console.log('test');
+
+//yargs(hideBin(process.argv))
+//  .command('serve [port]', 'start the server', (yargs) => {
+//    return yargs
+//      .positional('port', {
+//        describe: 'port to bind on',
+//        default: 5000
+//      })
+//  }, (argv) => {
+//    if (argv.verbose) console.info(`start server on :${argv.port}`)
+//    console.log(argv.port)
+//  })
+//  .option('verbose', {
+//    alias: 'v',
+//    type: 'boolean',
+//    description: 'Run with verbose logging'
+//  })
+//  .parse()
+//
+//inquirer
+//  .prompt([
+//    /* Pass your questions in here */
+//  ])
+//  .then((answers) => {
+//    // Use user feedback for... whatever!!
+//  })
+//  .catch((error) => {
+//    if (error.isTtyError) {
+//      // Prompt couldn't be rendered in the current environment
+//    } else {
+//      // Something else went wrong
+//    }
+//  });
 
 // 実際のフォントを読んで参考にするらしく、フォントのファイルが無いとできないっぽい
-// const figlet = require('figlet');
-// 
-// figlet.text('KNIW', { font: 'sans-serif' }, function(err: any, data: any) {
-//     if (err) {
-//         console.log('Something went wrong...');
-//         console.dir(err);
-//         return;
-//     }
-//     console.log(data)
-// });
+
+//figlet.text('KNIW', function(err: any, data: any) {
+//    if (err) {
+//        console.log('Something went wrong...');
+//        console.dir(err);
+//        return;
+//    }
+//    console.log(data)
+//});
 
 // const num : number = +process.argv[2];
 // console.log(fizzbuzz(num));
