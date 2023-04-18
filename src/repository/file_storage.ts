d504ca4859edca5df6262fd34a81ee0f2a5510b8
Takuya Sugiyama <motojouya@gmail.com>
2023-04-18 00:39:55 +0000

    file test

--- src/repository/file_storage.ts
+++ src/repository/file_storage.ts
@@ -1,19 +1,46 @@
 import fs from 'fs'
 
-async function createDirectory(dir: string): Promise<void> {
+type CreateDirectory = (dir: string) => Promise<void>;
+const createDirectory: CreateDirectory = async dir => {
   if (!fs.existsSync(dir)) {
-    return fs.promises.mkdir(dir);
+    return await fs.promises.mkdir(dir);
   }
 };
 
-async function createStorage(homedir: string): Promise<void> {
-  await createDirectory(homedir + '/.kniw');
-  await createDirectory(homedir + '/.kniw/charactor');
-  await createDirectory(homedir + '/.kniw/paty');
-  await createDirectory(homedir + '/.kniw/battle');
+type CreateStorage = (basedir: string) => Promise<void>;
+const createStorage: CreateStorage = async basedir => {
+  await createDirectory(basedir);
+  await createDirectory(basedir + '/charactor');
+  await createDirectory(basedir + '/paty');
+  await createDirectory(basedir + '/battle');
 };
 
-const homeDir: string | undefined = process.env.HOMEPATH || process.env.HOME;
+type Save = (fileName: string, text: string) => Promise<void>;
+const save: Save = async (fileName, text) => fs.promises.writeFile(fileName, text);
 
-createStorage(homeDir as string).then(r => console.log(r, 'dir created!'))
+type List = (dirName: string) => Promise<string[]>;
+const list: List = async dirName => fs.promises.readdir(dirName).then(files => files.filter(file => fs.statSync(dirName + '/' + file).isFile() && /.*\.json/.test(file)));
 
+async function test () {
+  const homeDir: string | undefined = process.env.HOMEPATH || process.env.HOME;
+  if (!homeDir) {
+      console.log('something err.');
+      return;
+  }
+  const baseDir: string = (homeDir as string) + '/.kniw';
+  await createStorage(baseDir);
+  console.log('dir created!')
+
+  const files = await list(baseDir + '/charactor');
+  console.log(files);
+  //const name: string = 'jonny';
+  //const charSetting: object = {
+  //  "weapon": "ax",
+  //  "armar": "blue",
+  //  "element": "fire",
+  //};
+  //await save(baseDir + '/charactor/' + name + '.json', JSON.stringify(charSetting));
+  //console.log('file created!');
+}
+
+test();
