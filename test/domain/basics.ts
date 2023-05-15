ab588a4570e18f5f483c4f840ed6ca0c6a7e5c20
Takuya Sugiyama <motojouya@gmail.com>
2023-05-15 00:40:15 +0000

    add ts-paths

--- test/domain/basics.ts
+++ test/domain/basics.ts
@@ -2,9 +2,90 @@ import assert from 'assert';
 import {
   changeClimate,
   addPhysicals,
-} from '../../src/domain/basics'; //TODO 絶対path
+} from 'src/domain/basics';
 
 describe('Climate#changeClimate', function () {
+  it('5 test', function () {
+    const result = changeClimate({
+      times: 0.1,
+      damage: 0.1,
+      accuracy: 0.05,
+    });
+    assert.equal(result, 'SUNNY');
+  });
+  it('15 test', function () {
+    const result = changeClimate({
+      times: 0.1,
+      damage: 0.1,
+      accuracy: 0.15,
+    });
+    assert.equal(result, 'SUNNY');
+  });
+  it('25 test', function () {
+    const result = changeClimate({
+      times: 0.1,
+      damage: 0.1,
+      accuracy: 0.25,
+    });
+    assert.equal(result, 'SUNNY');
+  });
+  it('35 test', function () {
+    const result = changeClimate({
+      times: 0.1,
+      damage: 0.1,
+      accuracy: 0.35,
+    });
+    assert.equal(result, 'SUNNY');
+  });
+  it('45 test', function () {
+    const result = changeClimate({
+      times: 0.1,
+      damage: 0.1,
+      accuracy: 0.45,
+    });
+    assert.equal(result, 'SUNNY');
+  });
+  it('55 test', function () {
+    const result = changeClimate({
+      times: 0.1,
+      damage: 0.1,
+      accuracy: 0.55,
+    });
+    assert.equal(result, 'RAIN');
+  });
+  it('65 test', function () {
+    const result = changeClimate({
+      times: 0.1,
+      damage: 0.1,
+      accuracy: 0.65,
+    });
+    assert.equal(result, 'RAIN');
+  });
+  it('75 test', function () {
+    const result = changeClimate({
+      times: 0.1,
+      damage: 0.1,
+      accuracy: 0.75,
+    });
+    assert.equal(result, 'FOGGY');
+  });
+  it('85 test', function () {
+    const result = changeClimate({
+      times: 0.1,
+      damage: 0.1,
+      accuracy: 0.85,
+    });
+    assert.equal(result, 'STORM');
+  });
+  it('95 test', function () {
+    const result = changeClimate({
+      times: 0.1,
+      damage: 0.1,
+      accuracy: 0.95,
+    });
+    assert.equal(result, 'SNOW');
+  });
+
   it('0 test', function () {
     const result = changeClimate({
       times: 0.1,
@@ -13,21 +94,33 @@ describe('Climate#changeClimate', function () {
     });
     assert.equal(result, 'SUNNY');
   });
-  it('10 test', function () {
+  it('40 test', function () {
     const result = changeClimate({
       times: 0.1,
       damage: 0.1,
-      accuracy: 0.1,
+      accuracy: 0.40,
     });
-    assert.equal(result, 'SUNNY');
+    assert.equal(result, 'SNOW');
   });
-  it('50 test', function () {
+  it('41 test', function () {
     const result = changeClimate({
       times: 0.1,
       damage: 0.1,
-      accuracy: 0.5,
+      accuracy: 0.41,
     });
-    assert.equal(result, 'RAIN');
+    assert.equal(result, 'SNOW');
   });
+  it('100 test', function () {
+    const result = changeClimate({
+      times: 0.1,
+      damage: 0.1,
+      accuracy: 1,
+    });
+    assert.equal(result, 'SNOW');
+  });
+
+  //TODO 数値以外の値の入力も
+  //マイナスとか、1を超えるとか、timesとdamageの値の変化とか
+
 });
 
