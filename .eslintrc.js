71917b9c1168cc13d4fe917a105c8445303d1d29
Takuya Sugiyama <motojouya@gmail.com>
2023-07-06 00:36:17 +0000

    eslint prettier configure

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.eslint.json",
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: ["dist"],
+  extends: [
+    "eslint:recommended",
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier"
+  ],
+  rules: {
+    "import/prefer-default-export": "off",
+  },
+};
