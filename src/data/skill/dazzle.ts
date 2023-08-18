8534dd194a79af41500bde740cebd70a56b3352e
Takuya Sugiyama <motojouya@gmail.com>
2023-08-18 14:49:22 +0000

    prettier

--- src/data/skill/dazzle.ts
+++ src/data/skill/dazzle.ts
@@ -6,7 +6,7 @@ export const dazzle: Skill = {
   name: 'dazzle',
   label: '幻惑',
   type: 'SKILL_TO_CHARACTOR',
-  action: ((skill, actor, randoms, field, receiver) => addStatus(accuracyDown)(skill, actor, randoms, field, receiver)),
+  action: (skill, actor, randoms, field, receiver) => addStatus(accuracyDown)(skill, actor, randoms, field, receiver),
   directType: DIRECT_TYPE_NONE,
   magicType: MAGIC_TYPE_NONE,
   baseDamage: 0,
--- src/data/skill/stoneWeather.ts
+++ src/data/skill/stoneWeather.ts
@@ -6,7 +6,8 @@ export const stoneWeather: Skill = {
   name: 'stoneWeather',
   label: '風化',
   type: 'SKILL_TO_CHARACTOR',
-  action: ((skill, actor, randoms, field, receiver) => addStatus(directDiffenceDown)(skill, actor, randoms, field, receiver)),
+  action: (skill, actor, randoms, field, receiver) =>
+    addStatus(directDiffenceDown)(skill, actor, randoms, field, receiver),
   directType: DIRECT_TYPE_NONE,
   magicType: MAGIC_TYPE_ROCK,
   baseDamage: 0,
