431b65851385854eeeef4b6e6ea814428292634c
Takuya Sugiyama @motojouya <motojouya@gmail.com>
2024-12-20 00:29:38 +0000

    working

--- src/store/store.ts
+++ src/store/store.ts
@@ -73,3 +73,73 @@ export class DataExistError {
 //   list: Object.keys(items as Dictionary<T>),
 //   all: Object.values(items as Dictionary<T>)
 // });
+
+// TODO 以下にも書き換えて差し替えたい。 working
+
+import type { CreateSave, CreateGet, CreateRemove, CreateList, CreateExportJson, CreateImportJson, CreateStore } from '@motojouya/kniw/src/store/store';
+import type { Battle } from '@motojouya/kniw/src/domain/battle';
+
+import { NotBattlingError } from '@motojouya/kniw/src/domain/battle';
+import { CharactorDuplicationError } from '@motojouya/kniw/src/domain/party';
+import { NotWearableErorr } from '@motojouya/kniw/src/domain/acquirement';
+import { JsonSchemaUnmatchError, DataNotFoundError } from '@motojouya/kniw/src/store/store';
+import { toBattleJson, toBattle, battleSchema } from '@motojouya/kniw/src/store/schema/battle';
+
+const NAMESPACE = 'battle';
+
+const createSave: CreateSave<Battle> = repository => async obj =>
+  repository.save(NAMESPACE, obj.title, toBattleJson(obj));
+
+type CreateGetBattle = CreateGet<
+  Battle,
+  NotWearableErorr | DataNotFoundError | CharactorDuplicationError | JsonSchemaUnmatchError | NotBattlingError
+>;
+const createGet: CreateGetBattle = repository => async name => {
+  const result = await repository.get(NAMESPACE, name);
+  if (!result) {
+    return null;
+  }
+
+  const battleJson = parseJson(battleSchema)(result);
+  if (battleJson instanceof JsonSchemaUnmatchError) {
+    return battleJson;
+  }
+
+  return toBattle(battleJson);
+};
+
+const createRemove: CreateRemove = repository => async name => repository.remove(NAMESPACE, name);
+
+const createList: CreateList = repository => async () => repository.list(NAMESPACE);
+
+const createExportJson: CreateExportJson<Battle> = repository => async (obj, fileName) =>
+  repository.exportJson(toBattleJson(obj), fileName);
+
+type CreateImportJson<S, M, J, E> = <S, M, J, E>(schema: S, toModel: ToModel<M, J, E>) => (repository) => ImportJson;
+const createImportJson: CreateImportJson = (schema, toModel) => (repository) => async (fileName) => {
+  const result = await repository.importJson(fileName);
+  if (!result) {
+    return null;
+  }
+
+  const json = parseJson(schema)(result);
+  if (json instanceof JsonSchemaUnmatchError) {
+    return json;
+  }
+
+  return toModel(json);
+};
+
+type ToModel<M, J, E> = (json: J) => M | E;
+type ToJson<M, J> = (model: M) => J;
+export const createStore: CreateStore<S, M, J, E> = (namespace: string, schema: S, toModel: ToModel<M, J, E>, toJson: ToJson<M, J>) => async (repository) => {
+  await repository.checkNamespace(namespace);
+  return {
+    save: createSave(repository),
+    list: createList(repository),
+    get: createGet(repository),
+    remove: createRemove(repository),
+    importJson: createImportJson(repository),
+    exportJson: createExportJson(repository),
+  };
+};
