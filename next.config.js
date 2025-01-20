de2d0ae0688e9ed5b643784e1ea2f34035ce094c
Takuya Sugiyama @motojouya <motojouya@gmail.com>
2025-01-20 15:23:36 +0000

    file changes

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
-module.exports = {
-  basePath: urlPrefix,
-  assetPrefix: urlPrefix,
-  trailingSlash: true,
-};
--- webpack.dev.js
+++
@@ -1,6 +0,0 @@
-const { merge } = require('webpack-merge'); // v5以上は { merge } = require('webpack-merge') とする
-const common = require('./webpack.config.js');
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-});
