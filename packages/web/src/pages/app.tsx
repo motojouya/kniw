ef4c0d39ce21e89654cb093ef6fd7f7f55fa27ff
Takuya Sugiyama @motojouya <motojouya@gmail.com>
2025-03-06 01:07:12 +0000

    a

--- packages/web/src/pages/app.tsx
+++ packages/web/src/pages/app.tsx
@@ -29,12 +29,27 @@ export const App: FC = () => (
           </Link>
         </List.Item>
       </List.Root>
-      <Text>更に詳しい説明は<a href="https://github.com/motojouya/kniw">こちらのページ</a>を参照してください。</Text>
+      <Text>遊び方やルールは<a href="https://github.com/motojouya/kniw">こちらのページ</a>を参照してください。</Text>
     </Box>
   </Box>
 );
 
+// containerはこんな感じ
+// div(full width and flex center)
+//   div container(max width 1024とか)
+// そもそもcontainerがあるからそっちで行けそう
+//
+// top 画面はあとリンクをちゃんとする感じかな
+// ゲームの準備, ゲームの進行の文言はいらないか。単純に箇条書きのほうがいい
+// 箇条書きではなく、ボタンが横に並んでるほうがいいんじゃないか。
+// 説明へのリンクは上に移動する。
+// ボタンを横に並べるのは、flexかな
+// その前に、contents自体をflexで囲って中央にしないと。それかcenterタグでいけるか？centerだと上下も中央になっちゃうね
+
 export const Header: React.FC<{ backLink: string; }> = ({ backLink }) => {
+  // header div(flex left align)
+  //   icon link
+  //   text link
   return (
       <header className="w-full max-w-5xl h-12 flex flex-nowrap justify-between items-center bg-white border-b">
         {backLink && (
