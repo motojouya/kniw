f05c5a14698de3eefc636d084a56b23cd1f58096
Takuya Sugiyama @motojouya <motojouya@gmail.com>
2025-03-07 01:13:11 +0000

    top flex box

--- packages/web/src/components/utility.tsx
+++ packages/web/src/components/utility.tsx
@@ -1,5 +1,15 @@
 /* eslint-disable react-refresh/only-export-components */
 import type { FC, ReactNode } from 'react';
+import {
+  Container as ChakraContainer,
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Icon,
+  Link as ChakraLink,
+} from '@chakra-ui/react';
+import { IoIosArrowBack } from "react-icons/io";
 
 const urlPrefix = import.meta.env.VITE_URL_PREFIX;
 
@@ -17,12 +27,74 @@ export const transit: Transit = (path) => {
 export type GetSearchParams = () => URLSearchParams;
 export const getSearchParams: GetSearchParams = () => new URLSearchParams(window.location.search);
 
-export const Link: FC<{ href: string, children: ReactNode }> = ({ href, children }) => {
+export const Link: FC<{ href: string, line: boolean | null, children: ReactNode }> = ({ href, line, children }) => {
 
   let assignPath = href;
   if (urlPrefix) {
     assignPath = "/" + urlPrefix + href;
   }
 
-  return (<a href={assignPath}>{children}</a>);
+  let variant = null;
+  if (line) {
+    variant = "underline";
+  }
+
+  return (<ChakraLink href={assignPath} variant={variant}>{children}</ChakraLink>);
 };
+
+export const App: FC = () => (
+  <Container>
+    <Flex direction="column" justify="flex-start" align="center">
+      <Text p="1">KNIWはタクティクスオウガを参考にしたボードゲームです。</Text>
+      <Text p="1">遊び方やルールは<ChakraLink href="https://github.com/motojouya/kniw" variant="underline">こちらのページ</ChakraLink>を参照してください。</Text>
+      <Flex p="1" justify="space-around" w="100%">
+        <Box>
+          <Link href="/party/" line>
+            <Text>パーティの作成</Text>
+          </Link>
+        </Box>
+        <Box>
+          <Link href="/battle/" line>
+            <Text>バトルの管理</Text>
+          </Link>
+        </Box>
+      </Flex>
+    </Flex>
+  </Container>
+);
+
+// top 画面はあとリンクをちゃんとする感じかな
+// ゲームの準備, ゲームの進行の文言はいらないか。単純に箇条書きのほうがいい
+// 箇条書きではなく、ボタンが横に並んでるほうがいいんじゃないか。
+// 説明へのリンクは上に移動する。
+// ボタンを横に並べるのは、flexかな
+// その前に、contents自体をflexで囲って中央にしないと。それかcenterタグでいけるか？centerだと上下も中央になっちゃうね
+
+export const Container: FC<{ backLink: string | null; children: ReactNode; }> = ({ backLink, children }) => (
+  <ChakraContainer>
+    <Box>
+      <Header backLink={backLink} />
+      {children}
+    </Box>
+  </ChakraContainer>
+);
+
+export const Header: FC<{ backLink: string | null; }> = ({ backLink }) => {
+  return (
+      <Flex p="3" justify="flex-start">
+        {backLink && (
+          <Box p="1">
+            <Icon fontSize="xl">
+              <IoIosArrowBack />
+            </Icon>
+          </Box>
+        )}
+        <Box p="1">
+          <Link href="/">
+            <Heading>KNIW</Heading>
+          </Link>
+        </Box>
+      </Flex>
+  );
+};
+
--- packages/web/src/pages/app.tsx
+++ packages/web/src/pages/app.tsx
@@ -1,70 +1,29 @@
-import type { FC } from 'react';
+import type { FC, ReactNode } from 'react';
 import {
   Box,
-  List,
-  Heading,
+  Flex,
   Text,
-  Icon,
+  Link as ChakraLink,
 } from '@chakra-ui/react';
-import { IoIosArrowBack } from "react-icons/io";
-import { Link } from '../components/utility';
+import { Container, Link } from '../components/utility';
 
 export const App: FC = () => (
-  <Box p='10'>
-    <Header />
-    <Box>
-      <Text>kniwは、Tactics Ogreを参考に作ったボードゲームです。</Text>
-      <Text>このアプリケーションでは以下のことを行うことができます。</Text>
-      <List.Root>
-        <List.Item>
-          <Text>ゲームの準備</Text>
-          <Link href="/party/">
-            <Text>Partyの作成</Text>
+  <Container>
+    <Flex direction="column" justify="flex-start" align="center">
+      <Text p="1">KNIWはタクティクスオウガを参考にしたボードゲームです。</Text>
+      <Text p="1">遊び方やルールは<ChakraLink href="https://github.com/motojouya/kniw" variant="underline">こちらのページ</ChakraLink>を参照してください。</Text>
+      <Flex p="1" justify="space-around" w="100%">
+        <Box>
+          <Link href="/party/" line>
+            <Text>パーティの作成</Text>
           </Link>
-        </List.Item>
-        <List.Item>
-          <Text>ゲームの進行</Text>
-          <Link href="/battle/">
-            <Text>Battleの管理</Text>
+        </Box>
+        <Box>
+          <Link href="/battle/" line>
+            <Text>バトルの管理</Text>
           </Link>
-        </List.Item>
-      </List.Root>
-      <Text>遊び方やルールは<a href="https://github.com/motojouya/kniw">こちらのページ</a>を参照してください。</Text>
-    </Box>
-  </Box>
+        </Box>
+      </Flex>
+    </Flex>
+  </Container>
 );
-
-// containerはこんな感じ
-// div(full width and flex center)
-//   div container(max width 1024とか)
-// そもそもcontainerがあるからそっちで行けそう
-//
-// top 画面はあとリンクをちゃんとする感じかな
-// ゲームの準備, ゲームの進行の文言はいらないか。単純に箇条書きのほうがいい
-// 箇条書きではなく、ボタンが横に並んでるほうがいいんじゃないか。
-// 説明へのリンクは上に移動する。
-// ボタンを横に並べるのは、flexかな
-// その前に、contents自体をflexで囲って中央にしないと。それかcenterタグでいけるか？centerだと上下も中央になっちゃうね
-
-export const Header: React.FC<{ backLink: string; }> = ({ backLink }) => {
-  // header div(flex left align)
-  //   icon link
-  //   text link
-  return (
-      <header className="w-full max-w-5xl h-12 flex flex-nowrap justify-between items-center bg-white border-b">
-        {backLink && (
-          <div className="grow shrink m-2">
-            <Icon fontSize="2xl" color="pink.700">
-              <HiHeart />
-            </Icon>
-          </div>
-        )}
-        <div className="grow shrink m-2">
-          <Link href="/">
-            <Heading>KNIW</Heading>
-          </Link>
-        </div>
-      </header>
-  );
-};
-
