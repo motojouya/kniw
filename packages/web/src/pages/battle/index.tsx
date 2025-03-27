626e3a3673be2a77d02a4cf9e8cf341ea0a6bc8e
Takuya Sugiyama @motojouya <motojouya@gmail.com>
2025-03-27 07:20:46 +0000

    delete chakra provider

--- packages/web/src/pages/battle/index.tsx
+++ packages/web/src/pages/battle/index.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from "../../components/ui/provider"
 import { App } from "./app"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <App />
   </StrictMode>,
 )
